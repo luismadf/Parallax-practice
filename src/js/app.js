3043e70cfa578c1238ddc12dbffb4a05837d6ec5
Luis De Freites <granjaw7@MacBook-Pro-de-GRANJAW7.local>
2022-02-15 22:20:19 +0100

    first commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,16 @@
-let bg = document.getElementById('bg')
-let moon = document.getElementById('moon')
-let mountain = document.getElementById('mountain')
-let road = document.getElementById('road')
-let text = document.getElementById('text')
+const layer1 = document.querySelector('.layer1')
+const layer2 = document.querySelector('.layer2')
+const layer3 = document.querySelector('.layer3')
+const layer4 = document.querySelector('.layer4')
+const layer5 = document.querySelector('.layer5')
+const layer6 = document.querySelector('.layer6')
+const layer7 = document.querySelector('.layer7')
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
-    bg.style.top = `${value * 0.5}px`
-    moon.style.left = `${-value * 0.5}px`
-    mountain.style.top = `${-value * 0.15}px`
-    road.style.top = `${value * 0.15}px`
-    text.style.top = `${value}px`
+    layer6.style.bottom = `${-value * 0.4}px`
+    layer5.style.bottom = `${-value * 0.3}px`
+    layer4.style.bottom = `${-value * 0.4}px`
+    layer3.style.bottom = `${-value * 0.3}px`
+    layer2.style.bottom = `${-value * 0.4}px`
 })
